b8103b06d1a7955c463fdb2f62dcf170f4a4c7a6
wenxianping <931128603@qq.com>
2018-12-22 15:31:44 +0800

    自动回复

--- weiChatToken/views.py
+++ weiChatToken/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
+import time
 from django.http.response import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt  # 解除csrf验证
 from wechat_sdk import WechatConf
 from wechat_sdk import WechatBasic
+
+# 微信服务器推送消息是xml的，根据利用ElementTree来解析出的不同xml内容返回不同的回复信息，就实现了基本的自动回复功能了，也可以按照需求用其他的XML解析方法
+import xml.etree.ElementTree as ET
 
 conf = WechatConf(  # 实例化配置信息对象
     token='weixin',  # 服务器配置-Token
@@ -19,12 +18,98 @@ conf = WechatConf(  # 实例化配置信息对象
 
 @csrf_exempt  # 去除csrf验证
 def wechat(request):
-    signature = request.GET.get('signature')  # 获取请求信息
-    timestamp = request.GET.get('timestamp')
-    nonce = request.GET.get('nonce')
-    wechat_instance = WechatBasic(conf=conf)  # 实例化微信基类对象
-    if not wechat_instance.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):  # 检查验证请求的签名
-        return HttpResponseBadRequest('Verify Failed')
+    if request.method == "GET":
+        signature = request.GET.get('signature')  # 获取请求信息
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+        wechat_instance = WechatBasic(conf=conf)  # 实例化微信基类对象
+        if not wechat_instance.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):  # 检查验证请求的签名
+            return HttpResponseBadRequest('Verify Failed')
+        else:
+            if request.method == 'GET':
+                return HttpResponse(request.GET.get('echostr', None))  # 返回请求中的回复信息
     else:
-        if request.method == 'GET':
-            return HttpResponse(request.GET.get('echostr', None))  # 返回请求中的回复信息
+        othercontent = autoreply(request)
+        return HttpResponse(othercontent)
+
+
+def autoreply(request):
+    try:
+        webData = request.body
+        xmlData = ET.fromstring(webData)
+
+        msg_type = xmlData.find('MsgType').text
+        ToUserName = xmlData.find('ToUserName').text
+        FromUserName = xmlData.find('FromUserName').text
+        CreateTime = xmlData.find('CreateTime').text
+        MsgType = xmlData.find('MsgType').text
+        MsgId = xmlData.find('MsgId').text
+
+        toUser = FromUserName
+        fromUser = ToUserName
+
+        if msg_type == 'text':
+            content = "您好,欢迎来到Python大学习!希望我们可以一起进步!"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            print("成功了!!!!!!!!!!!!!!!!!!!")
+            print(replyMsg)
+            return replyMsg.send()
+
+        elif msg_type == 'image':
+            content = "图片已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        elif msg_type == 'voice':
+            content = "语音已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        elif msg_type == 'video':
+            content = "视频已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        elif msg_type == 'shortvideo':
+            content = "小视频已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        elif msg_type == 'location':
+            content = "位置已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        else:
+            msg_type == 'link'
+            content = "链接已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+
+    except Exception as Argment:
+        return Argment
+
+
+class Msg(object):
+    def __init__(self, xmlData):
+        self.ToUserName = xmlData.find('ToUserName').text
+        self.FromUserName = xmlData.find('FromUserName').text
+        self.CreateTime = xmlData.find('CreateTime').text
+        self.MsgType = xmlData.find('MsgType').text
+        self.MsgId = xmlData.find('MsgId').text
+
+
+class TextMsg(Msg):
+    def __init__(self, toUserName, fromUserName, content):
+        self.__dict = dict()
+        self.__dict['ToUserName'] = toUserName
+        self.__dict['FromUserName'] = fromUserName
+        self.__dict['CreateTime'] = int(time.time())
+        self.__dict['Content'] = content
+
+    def send(self):
+        XmlForm = """
+        <xml>
+        <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+        <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+        <CreateTime>{CreateTime}</CreateTime>
+        <MsgType><![CDATA[text]]></MsgType>
+        <Content><![CDATA[{Content}]]></Content>
+        </xml>
+        """
+        return XmlForm.format(**self.__dict)
